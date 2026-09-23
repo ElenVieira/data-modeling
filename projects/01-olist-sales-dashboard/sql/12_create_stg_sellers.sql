7c75f7f389a353135e18ce5c227342ccd007f44a
ElenVieira <elenv.vieiracorporativo@gmail.com>
2026-09-23 14:54:28 -0300

    feat: add staging sellers table

---
+++ projects/01-olist-sales-dashboard/sql/12_create_stg_sellers.sql
@@ -0,0 +1,9 @@
+﻿-- Vendedores tratados para uso nas camadas analíticas
+-- Cleaned sellers for analytical layers
+
+CREATE TABLE IF NOT EXISTS staging.stg_sellers (
+    seller_id TEXT PRIMARY KEY,
+    seller_zip_code_prefix TEXT,
+    seller_city TEXT NOT NULL,
+    seller_state CHAR(2) NOT NULL
+);
