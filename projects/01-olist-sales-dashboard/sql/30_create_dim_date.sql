b8bd7723779b636e09de90cca49c7b17844b2073
ElenVieira <elenv.vieiracorporativo@gmail.com>
2026-09-24 15:54:43 -0300

    feat: add date dimension table

---
+++ projects/01-olist-sales-dashboard/sql/30_create_dim_date.sql
@@ -0,0 +1,15 @@
+﻿-- Dimensão calendário
+-- Calendar dimension
+
+CREATE TABLE IF NOT EXISTS marts.dim_date (
+    date_key INTEGER PRIMARY KEY,
+    full_date DATE NOT NULL UNIQUE,
+    day_of_month SMALLINT NOT NULL,
+    day_of_week SMALLINT NOT NULL,
+    week_of_year SMALLINT NOT NULL,
+    month_number SMALLINT NOT NULL,
+    quarter_number SMALLINT NOT NULL,
+    year_number SMALLINT NOT NULL,
+    year_month CHAR(7) NOT NULL,
+    is_weekend BOOLEAN NOT NULL
+);
