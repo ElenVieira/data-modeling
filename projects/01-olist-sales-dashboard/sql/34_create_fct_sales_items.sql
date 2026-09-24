75257305d34e50013519909754959ef6ac5fb4d4
ElenVieira <elenv.vieiracorporativo@gmail.com>
2026-09-24 16:04:37 -0300

    feat: add sales items fact table

---
+++ projects/01-olist-sales-dashboard/sql/34_create_fct_sales_items.sql
@@ -0,0 +1,28 @@
+﻿-- Fato analítica de itens vendidos
+-- Analytical sales items fact table
+
+CREATE TABLE IF NOT EXISTS marts.fct_sales_items (
+    sales_item_key BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    order_key BIGINT NOT NULL
+        REFERENCES marts.fct_orders (order_key),
+    order_item_id INTEGER NOT NULL,
+    customer_key BIGINT NOT NULL
+        REFERENCES marts.dim_customer (customer_key),
+    product_key BIGINT NOT NULL
+        REFERENCES marts.dim_product (product_key),
+    seller_key BIGINT NOT NULL
+        REFERENCES marts.dim_seller (seller_key),
+    purchase_date_key INTEGER NOT NULL
+        REFERENCES marts.dim_date (date_key),
+    shipping_limit_date_key INTEGER
+        REFERENCES marts.dim_date (date_key),
+
+    shipping_limit_timestamp TIMESTAMP,
+    price NUMERIC(12, 2) NOT NULL,
+    freight_value NUMERIC(12, 2) NOT NULL,
+    is_delivered BOOLEAN NOT NULL,
+
+    UNIQUE (order_key, order_item_id),
+    CHECK (price >= 0),
+    CHECK (freight_value >= 0)
+);
